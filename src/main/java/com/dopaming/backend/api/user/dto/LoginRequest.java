6b7bd7fde9e429d365ce71ceebfdaab5bea75a71
Jung-Na_Kyeong <jan022655@naver.com>
2026-05-11 21:29:41 +0900

    feat: swagger 설정 업데이트

--- src/main/java/com/dopaming/backend/api/user/dto/LoginRequest.java
+++ src/main/java/com/dopaming/backend/api/user/dto/LoginRequest.java
@@ -1,11 +1,16 @@
 package com.dopaming.backend.api.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class LoginRequest {
+
+    @Schema(description = "로그인 아이디", example = "testUser123")
     private String loginId;
+
+    @Schema(description = "비밀번호", example = "password123!")
     private String password;
 }
